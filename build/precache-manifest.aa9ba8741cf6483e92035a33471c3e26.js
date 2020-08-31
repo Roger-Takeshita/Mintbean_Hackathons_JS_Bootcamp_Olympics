e0d31221d71f19d21c6dbfa4c854625d0bbc510f
Roger Takeshita <roger.takeshita@gmail.com>
2020-08-31 03:04:44 -0400

    run build

---
+++ build/precache-manifest.aa9ba8741cf6483e92035a33471c3e26.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130a0ee7ec719e43785fafa0004fe779",
+    "url": "/index.html"
+  },
+  {
+    "revision": "001c81a26e5b1098832e",
+    "url": "/static/css/main.485a3e65.chunk.css"
+  },
+  {
+    "revision": "4bada5a0aae8b6781e7e",
+    "url": "/static/js/2.c4449e36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4449e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001c81a26e5b1098832e",
+    "url": "/static/js/main.6eb85155.chunk.js"
+  },
+  {
+    "revision": "e529daf1c17058dbf33c",
+    "url": "/static/js/runtime-main.7ede7b30.js"
+  },
+  {
+    "revision": "3096277f68583d5ec0390dc34cf55c25",
+    "url": "/static/media/000-board.3096277f.svg"
+  },
+  {
+    "revision": "9b0436d5dab24db2911ee0a9aa8bc097",
+    "url": "/static/media/001-ninja.9b0436d5.svg"
+  },
+  {
+    "revision": "9a5672349e9f5f00af7d69da5d0c4c1e",
+    "url": "/static/media/004-sticks.9a567234.svg"
+  },
+  {
+    "revision": "62d03110c5ec7b3e537ac8e57cad71f9",
+    "url": "/static/media/017-ninja-2.62d03110.svg"
+  },
+  {
+    "revision": "35b944068cdf23d0e136c31ebcb7fadc",
+    "url": "/static/media/018-dojo.35b94406.svg"
+  },
+  {
+    "revision": "764a8b75643769715f5ca9c6faeac877",
+    "url": "/static/media/020-kunai.764a8b75.svg"
+  },
+  {
+    "revision": "3b7401015da3b8406cb1feefdaf851d3",
+    "url": "/static/media/021-kunai-1.3b740101.svg"
+  },
+  {
+    "revision": "49088bdff86091aa94b278a0a35fe0d4",
+    "url": "/static/media/023-ninja-4.49088bdf.svg"
+  },
+  {
+    "revision": "5cc8c36d16cbdf6bdb47759dc7c164fe",
+    "url": "/static/media/026-scrolls.5cc8c36d.svg"
+  },
+  {
+    "revision": "2faccec055bcab205e06bc78e8bc9d55",
+    "url": "/static/media/035-ninja-6.2faccec0.svg"
+  },
+  {
+    "revision": "cceb51599f9eba43332f259aa37ca71d",
+    "url": "/static/media/042-scroll.cceb5159.svg"
+  },
+  {
+    "revision": "d3bebba97d549436fd6137d05d55ae33",
+    "url": "/static/media/DancingScript.d3bebba9.ttf"
+  },
+  {
+    "revision": "b79f920183ec1e232700b991c8ed3149",
+    "url": "/static/media/Shojumaru-6ZlM.b79f9201.ttf"
+  },
+  {
+    "revision": "34a3a491f52cbc52c43027ff801e9837",
+    "url": "/static/media/bamboo-2.34a3a491.jpg"
+  },
+  {
+    "revision": "11715d2dfdd2f9b7dc96a1648c96ebd4",
+    "url": "/static/media/ninja-1.11715d2d.png"
+  }
+]);
